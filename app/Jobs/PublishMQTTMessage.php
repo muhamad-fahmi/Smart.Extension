2c63fd6d03f22701a20eb4e2b308b25353083b1e
muhamad-fahmi <muhamadfahmi@outlook.com>
2024-05-29 22:12:30 +0700

    add try catch to job

--- app/Jobs/PublishMQTTMessage.php
+++ app/Jobs/PublishMQTTMessage.php
@@ -2,6 +2,8 @@
 
 namespace App\Jobs;
 
+use App\Models\Admin\Device;
+use App\Models\User\UserDevice;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
@@ -31,19 +33,34 @@ class PublishMQTTMessage implements ShouldQueue
      */
     public function handle(): void
     {
-        Log::info("Starting MQTT publish for topic: {$this->topic} with message: {$this->message}");
+        try {
+
+            Log::info("Starting MQTT publish for topic: {$this->topic} with message: {$this->message}");
 
 
-        $command = '/opt/homebrew/bin/mosquitto_pub -h '.env('MQTT_HOST').' -p '.env('MQTT_PORT').' -u '.env('MQTT_AUTH_USERNAME').' -P '.env('MQTT_AUTH_PASSWORD').' -t "'.$this->topic.'" -m "'.$this->message.'"';
+            $command = '/opt/homebrew/bin/mosquitto_pub -h '.env('MQTT_HOST').' -p '.env('MQTT_PORT').' -u '.env('MQTT_AUTH_USERNAME').' -P '.env('MQTT_AUTH_PASSWORD').' -t "'.$this->topic.'" -m "'.$this->message.'"';
 
-        $process = Process::fromShellCommandline($command);
-        $process->run();
+            $device_id = explode('/', $this->topic)[0];
+            $device = Device::where('device_id', $device_id)->first();
 
-        if (!$process->isSuccessful()) {
-            Log::error('MQTT publish failed: ' . $process->getErrorOutput());
-            throw new ProcessFailedException($process);
+            // dd($message);
+
+            UserDevice::where('device_id', $device->id)->update([
+                'last_status' => $this->message
+            ]);
+
+            $process = Process::fromShellCommandline($command);
+            $process->run();
+
+            if (!$process->isSuccessful()) {
+                Log::error('MQTT publish failed: ' . $process->getErrorOutput());
+                throw new ProcessFailedException($process);
+            }
+
+            \Log::info('Message published successfully: ' . $process->getOutput());
+
+        } catch (\Excetion $e) {
+            Log::error($e);
         }
-
-        \Log::info('Message published successfully: ' . $process->getOutput());
     }
 }
